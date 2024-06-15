6e008a8452d5520392648f8de2bd894937b8ee1d
duduFreire <dudufreiresantos@gmail.com>
2024-06-15 18:16:42 -0300

    fix template

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,11 +1,15 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
+#define int long long
 #define endl '\n'
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
 #define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
 #define debug(var) cout << #var << ": " << var << endl
+#define pb push_back
+#define eb emplace_back
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
--- stress-tests/utilities/template.h
+++ stress-tests/utilities/template.h
@@ -2,10 +2,13 @@
 using namespace std;
 
 #define endl '\n'
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
 #define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
 #define debug(var) cout << #var << ": " << var << endl
+#define pb push_back
+#define eb emplace_back
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
+
