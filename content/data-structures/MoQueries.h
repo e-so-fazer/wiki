5995f43fca2740ec0747684f4219f3ce4199b4c7
RuanPetrus <xastroboyx11@gmail.com>
2024-07-30 18:54:50 -0300

    Changing Mo queries to more generalize version

--- content/data-structures/MoQueries.h
+++ content/data-structures/MoQueries.h
@@ -6,26 +6,28 @@
  * Status: not stress_tested
  */
 
-vi mo(vector<pii> Q, vector<int> V) { // Queries in Q are [l, r)
+template<typename LS>
+void mo(LS & v, vector<pii> Q) { // Queries in Q are [l, r)
 	int L = 0, R = 0, blk = 350; // N/sqrt (Q)
 
-	vi s(sz(Q)), res = s;
+	vi s(sz(Q));
 	auto K = [&](pii x) {return pii(x.first/blk, x.second ^ -(x.first/blk & 1)); };
 	iota(all(s), 0);
 	sort(all(s), [&](int s, int t){ return K(Q[s]) < K(Q[t]); });
 
-	int sum = 0;
-	auto add =  [&](int ind, int end) { sum += V[ind]; }; // add a [ ind ] (end = 0 or 1)
-	auto del =  [&](int ind, int end) { sum -= V[ind]; }; // remove a [ ind ]
-	auto calc = [&]() { return sum; };                    // compute current answer
-
 	for (int qi : s) {
 		pii q = Q[qi];
-		while (L > q.first) add(--L, 0);
-		while (R < q.second) add(R++, 1);
-		while (L < q.first) del(L++, 0);
-		while (R > q.second) del(--R, 1);
-		res[qi] = calc();
+		while (L > q.first)  v.add(--L, 0);
+		while (R < q.second) v.add(R++, 1);
+		while (L < q.first)  v.del(L++, 0);
+		while (R > q.second) v.del(--R, 1);
+		v.calc(qi);
 	}
-	return res;
 }
+
+struct Spec {
+	using S = ll;
+	void add(int ind, int end) {}
+	void del(int ind, int end) {}
+	void calc(int idx) {}
+};
--- stress-tests/data-structures/MoQueries.cpp
+++ stress-tests/data-structures/MoQueries.cpp
@@ -6,6 +6,23 @@
 #include "../utilities/template.h"
 #include "../../content/data-structures/MoQueries.h"
 
+struct SumSpec {
+	using S = int;
+	vector<S> ans;
+	vector<int> v;
+	int sum = 0;
+	SumSpec(vector<int> values, int q): ans(q), v(values) {}
+	void add(int ind, int end) {
+		sum += v[ind];
+	}
+	void del(int ind, int end) {
+		sum -= v[ind];
+	}
+	void calc(int idx) {
+		ans[idx] = sum;
+	}
+};
+
 int main() {
 	rep(n,1,10) {
 		vector<pii> queries;
@@ -16,14 +33,14 @@ int main() {
 		}
 		vector<int> values(n);
 		rep(i, 0, n) values[i] = rand() % (n+5);
-		auto res = mo(queries, values);
+		SumSpec ls(values, sz(queries));
+		mo(ls, queries);
 		for (int i = 0; i < sz(queries); i++) {
 			auto [l, r] = queries[i];
 			int val = 0;
 			rep(k, l, r) val += values[k];
-			assert(val == res[i]);
+			assert(val == ls.ans[i]);
 		}
-
 	}
 	cout<<"Test passed!"<<endl;
 	return 0;
