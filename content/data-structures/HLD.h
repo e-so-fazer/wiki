542901a0da30de5c04b46b088b51a37024b0b57a
duduFreire <dudufreiresantos@gmail.com>
2024-08-24 19:38:41 -0300

    better HLD

--- content/data-structures/HLD.h
+++ content/data-structures/HLD.h
@@ -1,11 +1,15 @@
 /**
  * Author: Eduardo Freire
- * Description: Iterative, lazy, noncommutative HLD
- * Can be changed by modifying Spec, exactly like a segment tree
+ * Description: Iterative, lazy and noncommutative HLD
+ * Does exactly the same thing as a segment tree, but on a tree.
  * Time: query: O(\log^2 N), point update: O(\log N), range update: O(\log^2 N)
  * Status: stress-tested
  */
 
+
+// If T::op is commutative, memory usage can be cut in half by removing 'segi' from the code.
+// Simply remove it and replace every occurrence of segi.query(n-r, n-l) with seg.query(l,r).
+// Also remove every line involving updating segi. This also speeds up range updates by a factor of 2.
 template<class T>
 struct HLD {
 	using S = typename T::S;
@@ -13,10 +17,10 @@ struct HLD {
 	vvi g;
 	int rt,n,dt;
 	vi hv, ti,to, hd, p, ssz;
-	SegTree<T> seg;
+	SegTree<T> seg, segi;
 	HLD(vvi ag, int art=0) :
 		g(ag),rt(art),n(g.size()),dt(0),
-		hv(n),ti(n),to(n),hd(n),p(n), ssz(n),seg(n)
+		hv(n),ti(n),to(n),hd(n),p(n), ssz(n),seg(n), segi(n)
 	{
 		dfs1(rt,rt);
 		dfs2(rt,rt);
@@ -48,10 +52,6 @@ struct HLD {
 		return ti[a]<=ti[b] and to[a]>=to[b];
 	}
 
-	// if T::op is commutative then queryI can be replaced by query.	
-	// To implement seg.queryI, keep a second vector on the SegTree struct which will
-	// represent the segtree with operations swapped. Copy paste the query method
-	// and change every occorrence of T::op(a,b) with T::op(b,a). Similarly for the update.
 	// seg.query(l, r) must query [l, r).
 	// Query path from a to b, inclusive on both ends.
 	S query(int a, int b) {
@@ -59,31 +59,41 @@ struct HLD {
 		S a1=T::id(),a2=T::id();
 
 		for (;!isa(hd[a], b); a=p[hd[a]])
-			a1=T::op(a1, seg.queryI(ti[hd[a]], ti[a]+1));
+			a1=T::op(a1, segi.query(n-ti[a]-1, n-ti[hd[a]]));
 
 		for(;!isa(hd[b],a);b=p[hd[b]])
 			a2=T::op(seg.query(ti[hd[b]], ti[b]+1), a2);
 
 		if (isa(a,b)) a2=T::op(seg.query(ti[a], ti[b]+1), a2);
-		else a1=T::op(a1, seg.queryI(ti[b], ti[a]+1));
+		else a1=T::op(a1, segi.query(n-ti[a]-1, n-ti[b]));
 		return T::op(a1, a2);
 	}
 
-	// Remember that if T::op is not commutative, "update" must update the inverse seg as well.
 	void update(int a, K v) {
 		seg.update(ti[a], v);
+		segi.update(n-1-ti[a], v);
 	}
 	
 	// lazy update on path from a to b, inclusive on both ends.
 	void update(int a, int b, K v) {
-		for (;!isa(hd[a], b); a=p[hd[a]])
+		for (;!isa(hd[a], b); a=p[hd[a]]) {
 			seg.update(ti[hd[a]], ti[a]+1, v);
+			segi.update(n-ti[a]-1, n-ti[hd[a]], v);
+		}
 
-		for(;!isa(hd[b],a);b=p[hd[b]])
+		for(;!isa(hd[b],a);b=p[hd[b]]) {
 			seg.update(ti[hd[b]], ti[b]+1, v);
+			segi.update(n-ti[b]-1, n-ti[hd[b]], v);
+		}
 
-		if (isa(a,b)) seg.update(ti[a], ti[b]+1, v);
-		else seg.update(ti[b], ti[a]+1, v);
+		if (isa(a,b)) {
+			seg.update(ti[a], ti[b]+1, v);
+			segi.update(n-ti[b]-1, n-ti[a], v);
+		}
+		else {
+			seg.update(ti[b], ti[a]+1, v);
+			segi.update(n-ti[a]-1, n-ti[b], v);
+		}
 	}
 };
 
