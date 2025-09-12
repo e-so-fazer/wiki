65e09f09caf343a78d785eb99e31e28a23d56674
duduFreire <dudufreiresantos@gmail.com>
2025-09-12 15:48:14 -0300

    small changes to fft

--- content/math/fft.h
+++ content/math/fft.h
@@ -9,32 +9,29 @@ constexpr int MOD=998244353;
 
 int fpow(int a, int b) {
 	int x=1;
-	while(b) {
-		if (b&1) x=(ll)x*a%MOD;
-
-		a=(ll)a*a%MOD;
-		b/=2;
-	}
+	for (;b;a = (ll)a*a%MOD, b/=2) if (b&1) x = (ll) x*a % MOD;
 	return x;
 }
-
-void fft(vi &a) {
-	int n = sz(a), L = 31 - __builtin_clz(n);
-	static vi rt(2, 1);
-	for (static int k = 2, s = 2; k < n; k *= 2, s++) {
-		rt.resize(n);
-		ll z[] = {1, fpow(62, MOD >> s)};
-		rep(i,k,2*k) rt[i] = (ll) rt[i / 2] * z[i & 1] % MOD;
+ 
+void fft(vi& a) {
+	int n=sz(a), L=31-__builtin_clz(n);
+	static vi rt(2,1);
+	rt.resize(n);
+	for (static int k=2, s=2; k < n; k*=2, s++) {
+		ll z[] = {1,fpow(62, MOD >> s)};
+		rep(i,k,2*k) rt[i] = (ll) rt[i/2] * z[i&1] % MOD;
 	}
+ 
 	vi rev(n);
-	rep(i,0,n) rev[i] = (rev[i / 2] | (i & 1) << L) / 2;
+	rep(i,0,n) rev[i] =(rev[i/2] | (i&1) << L) / 2;
 	rep(i,0,n) if (i < rev[i]) swap(a[i], a[rev[i]]);
-	for (int k = 1; k < n; k *= 2)
-		for (int i = 0; i < n; i += 2 * k) rep(j,0,k) {
-			ll z = (ll) rt[j + k] * a[i + j + k] % MOD; int &ai = a[i + j];
-			a[i + j + k] = ai - z + (z > ai ? MOD : 0);
-			ai += (ai + z >= MOD ? z - MOD : z);
+	for (int k=1; k < n; k *=2) {
+		for (int i=0; i < n; i += 2*k) rep(j,0,k) {
+			ll z = (ll) rt[j+k] * a[i+j+k] % MOD; int& ai = a[i+j];
+			a[i+j+k] = ai - z + (z > ai ? MOD : 0);
+			ai += (ai + z >= MOD ? z-MOD : z);
 		}
+	}
 }
 vi mul(vi &a, vi &b) {
 	if (a.empty() || b.empty()) return {};
