8347d5af83d3905cbd905faafc271a3259c08b58
duduFreire <dudufreiresantos@gmail.com>
2026-02-04 15:26:14 -0300

    add CRT from kactl

--- content/math/ExtendedGCD.h
+++ content/math/ExtendedGCD.h
@@ -1,18 +1,15 @@
 /**
- * Author: Eduardo Freire
- * Description: Returns integers (d,x,y) such that ax + by = d and d=gcd(a,b)
- * Time: O(log(min(a,b)))
- * Status: not stress-tested
+ * Author: Unknown
+ * Date: 2002-09-15
+ * Source: predates tinyKACTL
+ * Description: Finds two integers $x$ and $y$, such that $ax+by=\gcd(a,b)$. If
+ * you just need gcd, use the built in \texttt{\_\_gcd} instead.
+ * If $a$ and $b$ are coprime, then $x$ is the inverse of $a \pmod{b}$.
  */
+#pragma once
 
-tuple<int,int,int> egcd(int a, int b) {
-	int ar=a, as=1,s=0,at=0,t=1;
-	for (int r=b; r;) {
-		int q = ar / r;
-		tie(ar, r) = pii(r,ar-q*r);
-		tie(as, s) = pii(s,as-q*s);
-		tie(at, t) = pii(t,at-q*t);
-	}
-	return {ar, as, at};
+ll euclid(ll a, ll b, ll &x, ll &y) {
+	if (!b) return x = 1, y = 0, a;
+	ll d = euclid(b, a % b, y, x);
+	return y -= a/b * x, d;
 }
-
